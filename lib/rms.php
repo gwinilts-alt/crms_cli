e13812c7ea033806c51711e4d59e888a35821cc5
gwinilts <gwionps@gmail.com>
2025-05-04 23:02:43 +0100

    consider local tracking of CRU to reduce throuput

--- lib/rms.php
+++ lib/rms.php
@@ -18,8 +18,8 @@ class RMS {
 
     private static function get($url) {
         $ch = curl_init($url);
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, ["Content-Type:application/json", "X-SUBDOMAIN:" . self::$sdbm, "X-AUTH-TOKEN:" . self::$token]);
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type:application/json", "X-SUBDOMAIN:" . self::$sdbm, "X-AUTH-TOKEN:" . self::$token]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch);
         return [
             "response" => json_decode($result, true),
@@ -49,9 +49,9 @@ class RMS {
         $ch = curl_init($url);
 
         $payload = json_encode( $data );
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, ["Content-Type:application/json", "X-SUBDOMAIN:" . self::$sdbm, "X-AUTH-TOKEN:" . self::$token]);
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type:application/json", "X-SUBDOMAIN:" . self::$sdbm, "X-AUTH-TOKEN:" . self::$token]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch);
         return [
             "response" => json_decode($result, true),
@@ -65,6 +65,44 @@ class RMS {
         self::assetsByPID($cf5["id"]);
 
     }
+
+    // Synchronizing RMS with QCCheck would involve deleting or ammending items that are in RMS to reflect the state of QCCheck
+    // Editing key information in RMS is not allowed.
+    // A succesful sync with RMS will gaurantee:
+    //  Items that do not exist in QCCheck will not exist in RMS
+    //  Items that have changed in QCCheck will be updated in RMS
+    //   
+
+    // the sync workflow would look like this:
+    
+    /**
+     * 1: get the Pp list from QCCheck
+     * Foreach member try to get a list of assets from RMS
+     * If there is a list:
+     *  For each list item find out if there is a QC item with the same asset number
+     *  if the Stock code is different, add to the alterations list
+     *  If there is no correlation, add to the removals list
+     * If there is no list:
+     *  Generate a new Product and NB the Stock_code
+     *  Associate all further items with this code
+     *  Later add all the items
+     * 
+     * 
+     * This should exclude colisions?
+     */
+
+     /**
+      * Fetching a Stock Level list is rate limited.
+      * Up to 100 items per page
+      * 60 pages per minute
+      * so 6000 items per minute if there is a big list
+
+      * You can fetch a stock-check list, but it will not contain items not counted during the stock check.
+
+
+      * Or there could be an RMS server that can perform up to some number of requests by looking after a queue
+      * It would group
+      */
 
     public static function sync() {
         $now = new DateTime();
